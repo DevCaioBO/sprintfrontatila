8aafd4110fe4eba856ff230d767ba56feb536aaa
DevCaioBO <caiobuenooliveira1210@gmail.com>
2025-04-03 17:03:17 -0300

    feat: anime-center

--- src/pages/AnimeCentralProject/AnimeCentralProject.jsx
+++ src/pages/AnimeCentralProject/AnimeCentralProject.jsx
@@ -8,9 +8,10 @@ export default function AnimeCentralProject() {
     console.log()
  },[animeData])
   return (
-    <div className='flex '>
-        
+    <div className='flex w-full justify-center py-10 rounded-xl object-cover min-h-lvh bg-gray-900'>
+      <div className='w-[300px] h-[300px]'>
     <img src={animeData.images?.webp.large_image_url} alt="" />
+    </div>
     </div>
   )
 }
